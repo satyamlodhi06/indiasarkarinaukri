2adf1d090ccc08c39ef4fa51a4e862adf5cd3924
Satyamlodhi05 <satyamnarwariya436Q@gmail.com>
2024-12-18 12:35:38 +0530

    header-section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors:{
+        'skyblue':'#5EB5D3',
+        'darkbule':'#172F5F',
+        'text-color':'#ffffff',
+      },
+      fontFamily:{
+        'montserrat':'Montserrat',
+        'poppin':'Poppins',
+      }
+    },
+  },
+  plugins: [],
+}
+
